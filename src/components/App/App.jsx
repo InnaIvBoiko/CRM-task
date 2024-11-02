3a1a63d50a642512d44be25f844065d18773da88
InnaIvBoiko <inna_boiko@libero.it>
2024-11-02 22:05:44 +0100

    init project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import css from './App.module.css';
+
+export default function App() {
+    return (
+        <main className={css.container}>
+                <h1 lang='en'>Hello world!!!</h1>
+        </main>
+    );
+}
